97f88e3b307e3e7b9626a8e7e2c680b78d91d4b4
Daniel Chen <chen.daniel2099@gmail.com>
2019-01-25 18:08:53 -0500

    copied server code and initialized angular front-end

---
+++ node/server/config/config.js
@@ -0,0 +1,29 @@
+  module.exports = {
+    development: {
+      username: "project-collab-admin",
+      password: "collab-project",
+      database: "project-collab-db",
+      host: "127.0.0.1",
+      port: 5432,
+      dialect: "postgres"
+    },
+    test: {
+      username: "project-collab-admin",
+      password: "collab-project",
+      database: "project-collab-db",
+      host: "127.0.0.1",
+      port: 5432,
+      dialect: "postgres"
+    },
+    production: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOSTNAME,
+      dialect: 'postgres',
+      dialectOptions: {
+          ssl: true
+      },
+      use_env_variable: 'DATABASE_URL'
+  }
+}

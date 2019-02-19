12de102e2ad59478923cd710d18edc95942fd57d
Daniel Chen <chen.daniel2099@gmail.com>
2019-02-19 10:04:35 -0500

    added testing module for login and login service

--- angular/src/app/login/login.component.spec.ts
+++ angular/src/app/login/login.component.spec.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login.component';
 import {MatCardModule,MatFormFieldModule,MatInputModule} from '@angular/material';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 
 describe('LoginComponent', () => {
@@ -22,7 +23,8 @@ describe('LoginComponent', () => {
         MatFormFieldModule,
         MatInputModule,
         RouterTestingModule,
-        NoopAnimationsModule
+        NoopAnimationsModule,
+        HttpClientTestingModule
       ]
     })
     .compileComponents();

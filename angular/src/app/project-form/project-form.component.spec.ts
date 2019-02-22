ef71aca084f4dc9ad9ebf9cf71f9dd51d40b9db5
Kelly Bell <kelly.bell@mail.utoronto.ca>
2019-02-22 08:20:12 -0500

    Get backbone of tests working

--- angular/src/app/project-form/project-form.component.spec.ts
+++ angular/src/app/project-form/project-form.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProjectFormComponent } from './project-form.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+
 
 describe('ProjectFormComponent', () => {
   let component: ProjectFormComponent;
@@ -8,7 +18,18 @@ describe('ProjectFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectFormComponent ]
+      declarations: [ProjectFormComponent],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        RouterTestingModule,
+        NoopAnimationsModule,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
   }));
--- angular/src/app/project-form/project-form.service.spec.ts
+++ angular/src/app/project-form/project-form.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProjectFormService } from './project-form.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 describe('ProjectFormService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: ProjectFormService = TestBed.get(ProjectFormService);

b16452e3bb0bf579936f94f4dadeb793cf802037
3370sohail <3370sohail@gmail.com>
2019-03-10 18:40:59 -0400

    added in new tables (note: make sure to run sequelize db:migrate)

---
+++ node/server/migrations/20190310221413-create-tag-to-project.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('tag_to_projects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      tag_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'tags',
+          key: 'id',
+          as: 'tag_id',
+        },
+      },
+      project_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'projects',
+          key: 'id',
+          as: 'project_id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('tag_to_projects');
+  }
+};

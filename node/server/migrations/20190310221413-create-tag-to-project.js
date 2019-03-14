9b6780fe955e428021a664cb7cd5945813bf2ea5
Daniel Chen <chen.daniel2099@gmail.com>
2019-03-14 07:55:16 -0400

    Sample Data update (#21)
    * added tag sample data and removed non-null constraints for created and updated times
    
    * merge conflict removed

--- node/server/migrations/20190310221413-create-tag-to-project.js
+++ node/server/migrations/20190310221413-create-tag-to-project.js
@@ -29,11 +29,11 @@ module.exports = {
         },
       },
       createdAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE
       },
       updatedAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE
       }
     });

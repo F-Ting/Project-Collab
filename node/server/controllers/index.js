775c5db5a3f4b80bcf7527b56585060f7c8eab1b
bellkelly <bellkelly@users.noreply.github.com>
2019-03-14 10:20:21 -0400

    [WIP] Add project tag endpoints (#23)
    * Add project tag endpoints
    
    * remove comment

--- node/server/controllers/index.js
+++ node/server/controllers/index.js
@@ -1,11 +1,13 @@
 const users = require('./users');
 const projects = require('./projects');
+const tag_to_project = require('./tag_to_project');
 const user_associations = require('./user_associations');
 const upload = require('./upload');
 
 module.exports = {
   users,
   projects,
+  tag_to_project,
   user_associations,
   upload
 };

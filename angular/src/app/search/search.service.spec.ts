1a7e42dad7494a0b71eb9f47fb8ca1e26a6af882
Matthew Jay Wong <matthew.jay.wong@gmail.com>
2019-03-15 15:21:36 -0400

    Fix search service tests

--- angular/src/app/search/search.service.spec.ts
+++ angular/src/app/search/search.service.spec.ts
@@ -1,12 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SearchService } from './search.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('SearchService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    })
+  );
 
   it('should be created', () => {
     const service: SearchService = TestBed.get(SearchService);
     expect(service).toBeTruthy();
+  });
+
+  it('should have a getUsers function', () => {
+    const service: SearchService = TestBed.get(SearchService);
+    expect(service.getUsers).toBeDefined();
+  });
+
+  it('should have a getProjects function', () => {
+    const service: SearchService = TestBed.get(SearchService);
+    expect(service.getProjects).toBeDefined();
   });
 });

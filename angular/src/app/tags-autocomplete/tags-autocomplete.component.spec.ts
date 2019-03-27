99bba2a2026e81994a03b14f68a504a85af5f7b0
Kelly Bell <kelly.bell@mail.utoronto.ca>
2019-03-27 19:30:44 -0400

    Fix frontend tests

--- angular/src/app/tags-autocomplete/tags-autocomplete.component.spec.ts
+++ angular/src/app/tags-autocomplete/tags-autocomplete.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TagsAutocompleteComponent } from './tags-autocomplete.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatAutocompleteModule, MatChipsModule, MatIconModule, MatFormFieldModule } from '@angular/material';
+import { ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('TagsAutocompleteComponent', () => {
   let component: TagsAutocompleteComponent;
   let fixture: ComponentFixture<TagsAutocompleteComponent>;
+  const formBuilder: FormBuilder = new FormBuilder();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TagsAutocompleteComponent ]
+      declarations: [TagsAutocompleteComponent],
+      providers: [{ provide: FormBuilder, useValue: formBuilder }],
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MatAutocompleteModule,
+        MatChipsModule,
+        MatIconModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        FormsModule
+      ]
     })
     .compileComponents();
   }));
@@ -16,6 +34,9 @@ describe('TagsAutocompleteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TagsAutocompleteComponent);
     component = fixture.componentInstance;
+    component.group = formBuilder.group({
+      tags: []
+    });
     fixture.detectChanges();
   });
 

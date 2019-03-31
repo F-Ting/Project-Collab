66a75864773e2c322994b1ca336ea9d42833ee0e
3370sohail <3370sohail@gmail.com>
2019-03-31 19:55:36 -0400

    set up simple calcuation with Pearson correlation rank and euclidean distance

--- node/server/controllers/rec.js
+++ node/server/controllers/rec.js
@@ -6,6 +6,9 @@ const Projects = require('../models').projects;
 const Users = require('../models').users;
 const Associations = require('../models').user_associations;
 const Sequelize = require('sequelize');
+const Correlation = require('correlation-rank');
+const Similarity = require( 'compute-cosine-similarity' );
+const Distance = require('euclidean-distance')
 
 const Op = Sequelize.Op;
 
@@ -36,6 +39,154 @@ function get_tags_count_project_list(porjects, res){
     }).catch((error) => res.status(400).send(error));
 }
 
+async function feature_matrix(cur_user_id) {
+    let feature_matrix_users = [];
+    let feature_matrix_projects = [];
+    let tags_count = []
+    let num_users =0;
+    let num_tags =0;
+    let num_projects =0;
+    try {
+        await Users.count(
+
+        ).then(c => {
+            num_users = c;
+
+        }).catch((error) => console.log(error));
+
+        await Tags.count(
+
+        ).then(c => {
+            num_tags = c;
+        }).catch((error) => console.log(error));
+        await Projects.count(
+
+        ).then(c => {
+            num_projects = c;
+        }).catch((error) => console.log(error));
+
+    } catch (err) {
+      // Rollback transaction if any errors were encountered
+      console.log(err)
+    }
+
+    console.log("HERERE",num_tags,num_users)
+    for(let i =0; i<num_tags;i++){
+        tags_count.push(0);
+    }
+    for(let j =0; j<num_users; j++){
+        let temp = [...tags_count];
+        let c = await users_feature_vector(j+1,temp);
+        feature_matrix_users.push(c)
+    }
+    for(let j =0; j<num_projects; j++){
+        let temp = [...tags_count];
+        let c = await porject_feature_vector(j+1,temp);
+        feature_matrix_projects.push(c)
+    }
+    console.log(feature_matrix_users);
+    //Similarity( feature_matrix_users[1], feature_matrix_users[j])
+    sim_structs = [];
+    for(let j =0; j<num_users; j++){
+        if(j != cur_user_id){
+            sim_structs.push({user_id: j,distance:Distance(feature_matrix_users[cur_user_id], feature_matrix_users[j]), pcor:Correlation.rank(feature_matrix_users[cur_user_id], feature_matrix_users[j])});
+        }
+    }
+    sim_structs.sort((a, b) => a.distance - b.distance);
+    for(let i =0; i<3;i++){
+
+    }
+    console.log(sim_structs);
+    console.log(feature_matrix_projects);
+
+
+}
+
+async function users_feature_vector(user_id, tags_count){
+    let projects = [];
+    try {
+        let associations = await Associations
+          .findAll({
+            where: {
+              user_id: user_id,
+            },
+            order: [
+              ['createdAt', 'DESC'],
+            ],
+            attributes: {exclude: ['createdAt', 'updatedAt', 'user_id'] },
+            raw: true
+          })
+
+          for ( let i in associations){
+              projects.push(associations[i].project_id)
+          }
+
+         let final =  await TagToProject.sequelize.query('SELECT tag_id, count(project_id) as tag_count FROM tag_to_projects WHERE project_id IN(:status) GROUP BY tag_id ORDER BY tag_count DESC',
+            { replacements: { status: projects }, type: TagToProject.sequelize.QueryTypes.SELECT }
+          )
+          for ( let i in final){
+              tags_count[final[i].tag_id -1] = parseInt(final[i].tag_count);
+          }
+
+          let my_tags = await TagToUser.findAll({
+              where: { user_id: user_id },
+              attributes: { exclude: ['createdAt', 'updatedAt', 'user_id', 'id'] },
+              raw: true
+          })
+          let count_projects = projects.length;
+          for ( let i in my_tags){
+             tags_count[my_tags[i].tag_id -1] += count_projects;
+          }
+     }  catch (err) {
+       // Rollback transaction if any errors were encountered
+       console.log(err)
+     }
+     return tags_count;
+}
+
+async function porject_feature_vector(project_id, tags_count){
+    let users = [];
+    try {
+        let associations = await Associations
+          .findAll({
+            where: {
+              project_id: project_id,
+            },
+            order: [
+              ['createdAt', 'DESC'],
+            ],
+            attributes: {exclude: ['createdAt', 'updatedAt', 'project_id'] },
+            raw: true
+          })
+
+          for ( let i in associations){
+              users.push(associations[i].user_id)
+          }
+
+         let final =  await TagToUser.sequelize.query('SELECT tag_id, count(user_id) as tag_count FROM tag_to_users WHERE user_id IN(:status) GROUP BY tag_id ORDER BY tag_count DESC',
+            { replacements: { status: users }, type: TagToProject.sequelize.QueryTypes.SELECT }
+          )
+          for ( let i in final){
+              tags_count[final[i].tag_id -1] = parseInt(final[i].tag_count);
+          }
+
+          let my_tags = await TagToProject.findAll({
+              where: { project_id: project_id },
+              attributes: { exclude: ['createdAt', 'updatedAt', 'project_id', 'id'] },
+              raw: true
+          })
+          let count_users = users.length;
+          for ( let i in my_tags){
+             tags_count[my_tags[i].tag_id -1] += count_users;
+          }
+     }  catch (err) {
+       // Rollback transaction if any errors were encountered
+       console.log(err)
+     }
+     return tags_count;
+}
+
+
 module.exports = {
     // List all tags for a user
     list(req, res) {
@@ -65,8 +216,8 @@ module.exports = {
                 raw: true
             })
             .then(tag => {
-                var result = [];
-                for ( var tag_id in tag){
+                let result = [];
+                for ( let tag_id in tag){
                     result.push(tag[tag_id].tag_id)
                 }
                 get_project_list_by_tags(result, res);
@@ -82,8 +233,8 @@ module.exports = {
                 raw: true
             })
             .then(tag => {
-                var result = [];
-                for ( var tag_id in tag){
+                let result = [];
+                for ( let tag_id in tag){
                     result.push(tag[tag_id].tag_id)
                 }
                 get_user_list_by_tags(result, res);
@@ -104,8 +255,9 @@ module.exports = {
           raw: true
         })
         .then((associations) => {
-            var result = [];
-            for ( var i in associations){
+            let c = feature_matrix(req.params.user_id -1);
+            let result = [];
+            for ( let i in associations){
                 result.push(associations[i].project_id)
             }
             get_tags_count_project_list(result, res);
